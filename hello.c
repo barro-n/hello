e0a7c7844bfea33d3097a875db9cd8026c8c0079
BenHeckens <bheckens2@husker.unl.edu>
2025-08-28 13:50:56 -0500

    updated hello.c

--- hello.c
+++ hello.c
@@ -9,5 +9,7 @@ int main(int argc, char **argv) {
 
     printf("Barron W:)\n");
     printf("Computer Science Major\n");
+    printf("My name is Ben Heckens\n");
+    printf("My major is Electrical Engineering\n");
 
 }
